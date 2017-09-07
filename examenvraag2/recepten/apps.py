49aecb0543d794ac00190f78aca5e8ea9d5438e9
yourivh <youri.vanheester@student.ap.be>
2017-09-07 15:22:56 +0200

    Django model recept created

---
+++ examenvraag2/recepten/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ReceptenConfig(AppConfig):
+    name = 'recepten'
---
+++ examenvraag2/recepten/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Recept(models.Model):
+    recept_name = models.CharField(max_length = 200)
+    cals = models.IntegerField()
+    ingredienten = models.CharField(max_length = 500)
+    b_tijd = models.CharField(max_length = 20)
