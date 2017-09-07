43b8ba30d65521bc9f9de65c7aab07ef38287ebe
yourivh <youri.vanheester@student.ap.be>
2017-09-07 15:46:03 +0200

    Views added

--- examenvraag2/recepten/models.py
+++ examenvraag2/recepten/models.py
@@ -6,3 +6,5 @@ class Recept(models.Model):
     cals = models.IntegerField()
     ingredienten = models.CharField(max_length = 500)
     b_tijd = models.CharField(max_length = 20)
+    def __str__(self):
+        return self.recept_name
--- examenvraag2/recepten/views.py
+++ examenvraag2/recepten/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
+from .models import Recept
 
-# Create your views here.
-from django.http import HttpResponse
+def list_all_recepten(request):
+    recepten = Recept.objects.all()
+    context = {'recepten': recepten}
+    return render(request, 'recepten/index.html', context)
 
+def search(request):
+    if request.method == 'POST':
+        recept_name = request.POST.get('name').title() # Capitalize name
+        try:
+            recept = Recept.objects.get(name=recept_name)
+            context = {'recepten': recepten}
+            return render(request, 'recepten/index.html', context)
+        except:
+            return render(request, 'recepten/index.html', None)
+    else:
+        return render(request, 'recepten/search.html', None)
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the recepten index.")
+def add(request):
+    if request.method == 'POST':
+        recept_name = request.POST.get('name').title() # Capitalize name
+        try:
+            a = Recept(name=recept_name)
+            a.save()
+            for r in recepten:
+                r = Recept(recept_name=recept, recept_id=a.id)
+                q.save()
+            return render(request, 'recepten/ok.html', None)
+        except:
+            return render(request, 'recepten/index.html', None)
+    else:
+        return render(request, 'recepten/add.html', None)
